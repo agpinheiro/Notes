c5f9b62b0c7fe7bc7737aba82fd5f34ad40b7fa6
agpinheiro <agpinheiro.ti@gmail.com>
2023-09-30 18:47:25 -0300

    fix_alter_name

--- ios/NotesTests/NotesTests.m
+++ ios/NotesTests/NotesTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TodoListTests : XCTestCase
+@interface NotesTests : XCTestCase
 
 @end
 
-@implementation TodoListTests
+@implementation NotesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
